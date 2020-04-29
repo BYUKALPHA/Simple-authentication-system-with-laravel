cf220e2ac92a02c260a857d0413b4391e845190e
Byukusenge Alphonse <alphonso2009@gmail.com>
2020-04-30 00:01:27 +0300

    Update authentication page

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
+public function __construct(){
+    $this->middleware('auth');
+}
+
+
 {
 public function index(){
     return view('auth.passwords.change');
