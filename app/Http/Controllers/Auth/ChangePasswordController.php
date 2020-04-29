97d060717b73ec9da0782b24ace5875879534cb2
Byukusenge Alphonse <alphonso2009@gmail.com>
2020-04-30 00:27:02 +0300

    Remove error in aunthentication page

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
-public function __construct(){
-    $this->middleware('auth');
-}
+
 
 
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
 public function index(){
     return view('auth.passwords.change');
 }
