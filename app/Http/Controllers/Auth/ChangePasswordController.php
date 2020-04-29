79eafc7a0e244d78793cee2f357a1fb3137480bb
Byukusenge Alphonse <alphonso2009@gmail.com>
2020-04-29 23:08:48 +0300

    Change password part 2

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -1,13 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
 {
 public function index(){
     return view('auth.passwords.change');
+}
+public function changepassword(Request $request){
+    $this->validate($request,[
+    'oldpassword' => 'required',
+    'password' => 'required|confirmed'
+]);
+$hashedPassword = Auth::user()->password;
+if (Hash::check($request->oldpassword,$hashedPassword)){
+$user = User::find(Auth::id());
+$user->password = Hash::make($request->password);
+$user->save();
+return redirect()->route('login')->with('successMsg', 'Password was successfully changed');
+}else{
+    return redirect()->back()->with('errorMsg', 'Password is invalid');
+}
 }
 }
